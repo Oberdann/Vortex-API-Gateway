765d091b15a42894d16eecc26de28c0a203b1534
Oberdann <oberdandeveloper@gmail.com>
2026-03-26 00:15:50 -0300

    feat: add swagger property for doc

--- src/gateways/users/dtos/input/update-user-dto.ts
+++ src/gateways/users/dtos/input/update-user-dto.ts
@@ -1,14 +1,27 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsOptional, IsEmail, MinLength } from 'class-validator';
 
 export class UpdateUserDto {
+  @ApiProperty({
+    example: 'Maria da Silva',
+    description: 'Novo nome do usuário',
+  })
   @IsOptional()
   @IsString({ message: 'O campo [name] precisa ser uma string.' })
   name?: string;
 
+  @ApiProperty({
+    example: 'mariadasilva@email.com',
+    description: 'Novo e-mail do usuário',
+  })
   @IsOptional()
   @IsEmail({}, { message: 'O campo [email] precisa ser um e-mail válido.' })
   email?: string;
 
+  @ApiProperty({
+    example: '654321',
+    description: 'Nova senha do usuário',
+  })
   @IsOptional()
   @IsString({ message: 'O campo [password] precisa ser uma string.' })
   @MinLength(6, {
